21554c80ff12929072ccea9aaf378b6847a0ca57
mdantuono <mikeishere3@gmail.com>
2020-05-23 20:14:00 -0400

    now counts down 10 seconds..

--- Modules/InfoModule.cs
+++ Modules/InfoModule.cs
@@ -1,5 +1,7 @@
 using Discord.Commands;
 using System.Threading.Tasks;
+using System.Timers;
+using System;
 
 public class InfoModule : ModuleBase<SocketCommandContext>
 {
@@ -7,4 +9,29 @@ public class InfoModule : ModuleBase<SocketCommandContext>
 	[Summary("Echoes a message.")]
 	public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
 		=> ReplyAsync(echo);
+
+	[Command("start")]
+	[Summary("Starts the timer.")]
+	public async Task StartAsync() {
+
+		TimeSpan t = TimeSpan.FromMilliseconds(10000);
+		string defaultTime = string.Format($"{t.Minutes:D2}m:{t.Seconds:D2}s");
+
+		await ReplyAsync($"{defaultTime}. The clock's tickin'.");
+		var timer = new Timer(1000);
+		timer.Elapsed += HandleTimerElapsed;
+		timer.Start();
+
+		await Task.Delay(t);
+		timer.Stop();
+		await ReplyAsync("Next cronie.");
+	}
+
+	[Command("yeet")]
+	public Task Yeet() => ReplyAsync("Get outta hea!!");
+
+	public void HandleTimerElapsed(object sender, ElapsedEventArgs e)
+	{
+		ReplyAsync(".");
+	}
 }
